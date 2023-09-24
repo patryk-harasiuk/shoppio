2cbd859ffe03f54289970da2a34f6267deed6aef
patrykharasiuk <patryk.harasiuk@op.pl>
2023-09-24 21:43:11 +0200

    feat: add styles to root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { ProductList } from "@/ui/organisms/ProductList";
+import { type ProductItemType } from "@/ui/types";
+
+const products: ProductItemType[] = [];
+
 export default function Home() {
-	return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+	return <ProductList products={products} />;
 }
