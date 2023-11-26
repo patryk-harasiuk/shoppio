478540fabb19ba38aa0818450693a583b900fbf1
patrykharasiuk <patryk.harasiuk@op.pl>
2023-11-26 08:58:32 +0100

    feat: make executeGraphql function accept second argument only when needed

--- src/utils/executeGraphql.ts
+++ src/utils/executeGraphql.ts
@@ -6,7 +6,7 @@ type GraphQLResponse<T> =
 
 export const executeGraphql = async <TResult, TVariables>(
 	query: TypedDocumentString<TResult, TVariables>,
-	variables: TVariables,
+	...[variables]: TVariables extends Record<string, never> ? [] : [TVariables]
 ): Promise<TResult> => {
 	if (!process.env.GRAPHQL_URL) {
 		throw TypeError("GRAPHQL_URL is not defined");
